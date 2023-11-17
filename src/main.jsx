afcadf0b33b0181e6add4cea6b4a5e32115fdc30
Aniruddha Adak <adakaniruddha9@gmail.com>
2023-11-17 09:14:47 +0530

    Add useEffect in react

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import HookCounterOne from './useEffect.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <HookCounterOne/>
   </React.StrictMode>,
 )
---
+++ src/useEffect.jsx
@@ -0,0 +1,20 @@
+//HookCounterOne.js File 
+import { useState, useEffect } from "react"; 
+
+function HookCounterOne() { 
+	const [count, setCount] = useState(0); 
+
+	useEffect(() => { 
+		document.title = `You clicked ${count} times`; 
+	}, [count]); 
+
+	return ( 
+		<div> 
+        <h1>Counter Button :👇 </h1>
+			<button onClick={() => setCount((prevCount) => prevCount + 1)}> 
+				Click {count} times{" "} 
+			</button> 
+		</div> 
+	); 
+} 
+export default HookCounterOne;
