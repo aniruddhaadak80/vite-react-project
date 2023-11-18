55a245eccbcba54e1f4c3a57ad1b05161dc2b598
Aniruddha Adak <adakaniruddha9@gmail.com>
2023-11-18 11:30:32 +0530

    tic-tac-toe game

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import HookCounterOne from "./useEffect.jsx";
+import Board from "./game.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    {/* square for tic-tac-toe game  */}
+    <Board />
+    {/* for app.jsx */}
     <App />
+    {/* for  useEffect hooks  */}
     <HookCounterOne />
   </React.StrictMode>
 );
